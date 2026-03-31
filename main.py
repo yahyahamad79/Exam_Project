be72193e0b9ab5da81b69235791facb094fa4e0e
Yahya Hamad <yahyahamad79@gmail.com>
2026-03-31 22:16:06 +0300

    Reformat main.py with consistent indentation

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
- from fastapi import FastAPI, UploadFile, File
+from fastapi import FastAPI, UploadFile, File
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import FileResponse
 import fitz  # PyMuPDF
